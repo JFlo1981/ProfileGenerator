e6f2f2e5a6bdf695c4e6fbe6fb42b114676e3b74
Jarod Florence <jarod.florence@outlook.com>
2022-03-10 11:12:29 -0600

    lib.js files setup

---
+++ index.js
@@ -0,0 +1,106 @@
+/*
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for my team members and their information
+THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+WHEN I click on an email address in the HTML
+THEN my default email program opens and populates the TO field of the email with the address
+WHEN I click on the GitHub username
+THEN that GitHub profile opens in a new tab
+WHEN I start the application
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I enter the team manager’s name, employee ID, email address, and office number
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+WHEN I select the engineer option
+THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+WHEN I select the intern option
+THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+WHEN I decide to finish building my team
+THEN I exit the application, and the HTML is generated 
+*/
+
+
+/*
+const <> = require();
+        "   " 
+        "   "
+        "   "
+*/
+
+const managerSetup = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'What is the name of the Team Manager?  (required)',
+            validate: managerInput => {
+                if (managerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the Team Manager!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'What is the Employee ID number?  (required)',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Employee ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email address?  (required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is the office number?  (required)',
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the office number!');
+                    return false;
+                }
+            }
+        }
+    ])
+};
+
+/* 
+.then create this card
+*/
+
+const createTeam = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'teamRole',
+            message: "Please select the employee's role on the team.",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+
+        }
+    ])
+}
+
+// when: (input) => input.teamRole === "Engineer"
+// when: (input) => input.teamRole === "Intern"
+// to determine last question for each
